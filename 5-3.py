be1229a850583abe281f04e6ebdd81ecd04150f3
shapka.pv <shapka.pa@phystech.edu>
2021-10-19 14:44:31 +0300

    rcx

--- 5-3.py
+++ 5-3.py
@@ -1,52 +1,72 @@
-im import time
-import math
-leds = [24, 25, 8, 7, 12, 16, 20, 21]
+import matplotlib.pyplot as plt
+import RPi.GPIO as GPIO
+import time
+
 dac = [26, 19, 13, 6, 5, 11, 9, 10]
+leds = [21, 20, 16, 12, 7, 8, 25, 24]
 bits = len(dac)
 levels = 2**bits
 maxVoltage = 3.3
 TroykaMoudle = 17
 comparator = 4
+val = []
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(dac, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(dac + leds, GPIO.OUT, initial = GPIO.LOW)
 GPIO.setup(TroykaMoudle, GPIO.OUT, initial = GPIO.HIGH)
 GPIO.setup(comparator, GPIO.IN)
-GPIO.setup(leds, GPIO.OUT, initial = GPIO.LOW)
+
 def decimal2binary(i):
     return [int(elem) for elem in bin(i)[2:].zfill(bits)]
+
 def bin2dac(i): 
      signal = decimal2binary(i)
      GPIO.output(dac, signal)
-     return signal   
+     return signal
+
 def adc():
-    for i in range(8):
+    value = 0
+    for i in range(7, -1, -1):
         bin2dac(value + 2 ** i)
-        time.sleep(0.0005)
+        time.sleep(0.0008)
         comparatorValue = GPIO.input(comparator)
-        if i == 8:
-            if comparatorValue == 1:
-                value -= 1
-        else:
-            if comparatorValue == 0:
-                value += 2 ** i
-            else:
-                value -= 2 ** (i - 1) + 2 ** i
+        if comparatorValue == 1:
+            value += 2 ** i
+    return value
+
 try:
+    chek = True
+    GPIO.output(TroykaMoudle, 1) #Подаем питание на тройку-модуль
+    start = time.time() #Засекаем начало эксперимента
+    print('Началась зарядка конденсатора.')
     while True:
-        value = adc()
-        voltage = maxVoltage / levels * value 
-        print("analog value =  {:^3}, analog VOLTAGE = {:.2f}".format(value, voltage))
-        lamp = math.ceil(value/(bits)
-        for i in range(8):
-            if i < lamp:
-                GPIO.output(leds[i], 1)
-            else:
-                GPIO.output(leds[i], 0)
+        value = adc() #Считываем аналоговый сигнал с конденсатора
+        GPIO.output(leds, decimal2binary(value)) #Отображаем значение value на панели leds
+        if value >= 245 and chek: #Если зарядились, то отключаем питание
+            GPIO.output(17, 0)
+            print('Началась разрядка конденастора')
+            chek = False
+        if chek == False and value <= 5: #Если зарядились, то заканчиваем эксперимент
+            end = time.time() #засекаем конец эксперимента
+            T = (end - start)/(len(val)-1) #Считаем период
+            ny = 1/T #Считаем частоту
+            break
+        val.append(value) #Добавляем значение в список всех аналоговых значений 
+        voltage = maxVoltage / levels * value
+        print("digital value =  {:^3}, analog VOLTAGE = {:.2f}".format(value, voltage))
+    
+    plt.plot(val)
+    with open('data.txt', 'w') as f1:
+        for i in range(len(val)):
+            f1.write(str(val[i]) + '\n')          
+    with open('settings.txt', 'w') as f2:
+        f2.write('T = ' + str(T) + '\n')
+        f2.write('Ny = '+ str(ny))
+        
+    plt.show()
+    
 except KeyboardInterrupt:
-    print("programma ostanovlena klavoy")
-else:
-    print("Nikakix isklucheniy")
+    print("Прервано")
 finally:
-    GPIO.output(dac, GPIO.LOW)
+    GPIO.output(dac + leds, GPIO.LOW)
     GPIO.cleanup(dac)
-    print("GPIO cleanup completed")
